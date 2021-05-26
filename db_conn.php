9d02706a7ae8d8f3597a051bd5074b53f3583d2a
abjodas <47302962+abjodas@users.noreply.github.com>
2021-05-26 12:47:47 +0200

    Add files via upload

--- db_conn.php
+++ db_conn.php
@@ -1,12 +1,14 @@
 <?php
 
-$sname= "localhost";
-$unmae= "root";
-$password = "";
-
-$db_name = "test_db";
-
-$conn = mysqli_connect($sname, $unmae, $password, $db_name);
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 if (!$conn) {
 	echo "Connection failed!";
